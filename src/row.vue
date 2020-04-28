f655cdd6c5ee8c3a2bbee033306fb78468911e04
yll13454 <451492304@qq.com>
2020-04-29 07:16:02 +0800

    在row上添加对齐属性align

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="rowstyle" >
+    <div class="row" :style="rowstyle" :class="rowclass" >
         <slot></slot>
     </div>
 </template>
@@ -8,7 +8,13 @@
     export default {
         name:'LunziRow',
         props:{
-            gutter:[String,Number]
+            gutter:[String,Number],
+            align:{
+                type:String,
+                validator(value){
+                    return ['left','right','center'].includes(value)
+                }
+            }
         },
         mounted() {
             this.$children.forEach((vm)=>{
@@ -19,6 +25,12 @@
             rowstyle(){
                 let { gutter } = this;
                 return {marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'};
+            },
+            rowclass(){
+                let { align } = this;
+                console.log(align);
+                
+                return [ align && `align-${align}` ];
             }
         },
     }
@@ -27,5 +39,14 @@
 <style lang="scss" scoped>
 .row{
     display: flex;
+    &.align-left{
+        justify-content: flex-start;
+    }
+    &.align-right{
+        justify-content: flex-end;
+    }
+    &.align-center{
+        justify-content: center;
+    }
 }
 </style>
